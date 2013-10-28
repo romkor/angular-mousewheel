1a1eb43716e16a2293b7c1e3a90c20a807a18c1d
Roman Rodych <romko.ua@gmail.com>
2013-10-28 11:32:35 +0200

    Updated demo.

--- demo/scripts.js
+++ demo/scripts.js
@@ -1,23 +1,23 @@
 angular.module('Project', ['eveWheel']);
 
-function MyCntrl ($scope){
+function MyCntrl($scope) {
     $scope.colors = [
-        {name:'black', shade:'dark'},
-        {name:'white', shade:'light'},
-        {name:'red', shade:'dark'},
-        {name:'blue', shade:'dark'},
-        {name:'yellow', shade:'light'}
+        {name: 'black', shade: 'dark'},
+        {name: 'white', shade: 'light'},
+        {name: 'red', shade: 'dark'},
+        {name: 'blue', shade: 'dark'},
+        {name: 'yellow', shade: 'light'}
     ];
     $scope.color = $scope.colors[2]; // red
 
-    $scope.changeValue = function($delta){
+    $scope.changeValue = function ($delta) {
 
-        var current   = $scope.colors.indexOf($scope.color),
+        var current = $scope.colors.indexOf($scope.color),
             lastColor = $scope.colors.length - 1;
 
-        if ($delta > 0 && current == lastColor) {
+        if ($delta > 0 && current === lastColor) {
             $scope.color = $scope.colors[0];
-        } else if ($delta < 0 && current == 0) {
+        } else if ($delta < 0 && current === 0) {
             $scope.color = $scope.colors[lastColor];
         } else {
             $scope.color = $scope.colors[current + $delta];
@@ -25,9 +25,13 @@ function MyCntrl ($scope){
 
     }
 
-    $scope.getClass = function($index){
-        if ($scope.colors.indexOf($scope.color) == $index){
+    $scope.getClass = function ($index) {
+        if ($scope.colors.indexOf($scope.color) === $index) {
             return 'active';
         }
+    }
+
+    $scope.selectColor = function($index){
+        $scope.color = $scope.colors[$index];
     }
 }
